0d35d7817a87404bfe9ab33af394b021ef0a8a67
Tomas Votroubek <tomas.votroubek@outlook.com>
2024-10-31 23:23:38 +0100

    implement json loading

--- src/cgt_bandits/nodes.py
+++ src/cgt_bandits/nodes.py
@@ -8,17 +8,17 @@ class EFGNode:
     name: str
 
     def __repr__(self):
-        json.dumps(asdict(self))
+        return json.dumps(asdict(self))
 
 
-@dataclass
+@dataclass(repr=False)
 class ChanceNode(EFGNode):
     children: List[EFGNode]
     action_names: List[str]
     action_probs: List[float]
 
 
-@dataclass
+@dataclass(repr=False)
 class PersonalNode(EFGNode):
     infoset: int
     player: int
@@ -26,6 +26,16 @@ class PersonalNode(EFGNode):
     action_names: List[str]
 
 
-@dataclass
+@dataclass(repr=False)
 class TerminalNode(EFGNode):
     payoffs: List[float]
+
+
+def json_object_hook(dct):
+    if 'payoffs' in dct:
+        return TerminalNode(**dct)
+    elif 'action_probs' in dct:
+        return ChanceNode(**dct)
+    elif 'player' in dct:
+        return PersonalNode(**dct)
+    return dct
