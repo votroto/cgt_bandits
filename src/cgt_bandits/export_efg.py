5f7bb8af798dac2dc7e9b086f18917e0083cb70a
Tomas Votroubek <tomas.votroubek@outlook.com>
2024-11-01 02:57:51 +0100

    fract

--- src/cgt_bandits/export_efg.py
+++ src/cgt_bandits/export_efg.py
@@ -1,7 +1,4 @@
-from cgt_bandits.utils import fixup_nodes
-
-
 def nodes_to_efg(root, players, name=""):
     # EFG is too slow for BRUTE, let's try this instead.
 
-    return fixup_nodes(root)
+    return root
